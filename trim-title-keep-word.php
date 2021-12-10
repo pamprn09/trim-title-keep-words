37743f0758f23be78dffb66b13d3f280b3f80d34
Pamela Ribeiro <55361215+pamprn09@users.noreply.github.com>
2021-12-10 12:20:27 -0300

    Created file

---
+++ trim-title-keep-word.php
@@ -0,0 +1,17 @@
+function trim_title_keep_words($title, $max_lenght, $more)  {
+    // Title will remain untouched if character counut is smaller than max lenght
+    // O título permanecerá intacto se a quantidade de caracteres for menor que o máximo estabeleido.
+    if (strlen($title) <= $max_lenght) {
+        return $title;
+    }
+    
+    // Title is trimmed and word are kept intact
+    // O título é cortado mas a última palavra permanece inteira                               
+    $trimmed_title = substr($title, 0, $max_lenght);
+    if ( substr($trimmed_title,-1,1) != ' ' ) {
+        $trimmed_title = substr($trimmed_title, 0, strrpos($trimmed_title, " "));
+    }
+    // Adding read more to final                              
+    $trimmed_title = $trimmed_title.$more;
+    return $trimmed_title;
+}                                                                         
